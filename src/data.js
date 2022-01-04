17494e59c688bdd25b77fc7a067621782aa28104
Администратор <medinaisabekk@gmail.com>
2022-01-04 15:18:17 +0600

    Add project

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://i1.rgstatic.net/ii/profile.image/443267213991937-1482694547928_Q512/Ruslan-Isaev-4.jpg',
+    name: 'Ruslan Isaev',
+    title: 'Software developer',
+    quote:
+      'Ярый любитель бизнес идей и иновационных технологий. 2 года преподает в университете Ала-Тоо. Хороший человек.',
+  },
+  {
+    id: 2,
+    image:
+      'https://scontent.ffru7-1.fna.fbcdn.net/v/t1.6435-9/78699156_586694675410777_1229417552208199680_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=aNAAeCatGYMAX8TmpR2&_nc_ht=scontent.ffru7-1.fna&oh=00_AT-O_w0D6BXv0wiufmmad_Z5V8oi27cICdBg1foIp63IRA&oe=61FB68CF',
+    name: 'Medina Isabekova',
+    title: 'Project Manager',
+    quote:
+      'Хороший лидер и человек. Любит бизнес идеи, а особенно доход от них. Имеет цель поехать в кругосветное путешествие.',
+  },
+  {
+    id: 3,
+    image:
+      'https://citaty.info/files/characters/12386.png',
+    name: 'Azat Hazmatik',
+    title: 'Game developer',
+    quote:
+      'Любит смешарики и предпочитает делать игры. Кушает по расписанию и рост 2 метра.',
+  },
+  {
+    id: 4,
+    image:
+      'https://lh3.googleusercontent.com/proxy/QSAfckZqzlka58IDIKpPizCGy3KFtNpU22K_W1-MKRSp-mCPjviQfNhb8C2MqTAkjKBIDa982A9vj4yuKEI2ZpAcMSEyg44Mhv8jCQGtfrATim0s_E3Q2OTaZhOc99mMUYXWuTNXOzDebDoIKeOMcbXBGm2jhmXhSvYB5X6z4sO6_MLSZrEz1ncTIyzjioFlC9kbpgDqFzqpxnlvyWSGCZ5-TiVS-cDp55VIxICMA4R6NDzxWwamKQ',
+    name: 'Burul Shambetova',
+    title: 'FrontEnd Developer',
+    quote:
+      'Очень хоршая женщина и учитель. Имеет прекрасную ауру и источает лучик света.',
+  },
+];
+export default people;
